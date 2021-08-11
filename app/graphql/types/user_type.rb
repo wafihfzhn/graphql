1d592b1e7a07ddd987b40c1e484485955dded38a
wafihfzhn <hafizhuddin96@gmail.com>
2021-08-11 10:35:24 +0800

    Add user mutation

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,7 +3,13 @@ module Types
     field :id, ID, null: false
     field :email, String, null: true
     field :name, String, null: true
+    field :posts, [Types::PostType], null: true
+    field :posts_count, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def posts_count
+      object.posts.size
+    end
   end
 end
